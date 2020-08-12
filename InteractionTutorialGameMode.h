44f8ed94a37ebc564388572afb86751fb56cc0b0
LazyKatGames <69491090+LazyKatGames@users.noreply.github.com>
2020-08-11 20:21:02 -0500

    All source code files added

---
+++ InteractionTutorialGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "InteractionTutorialGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AInteractionTutorialGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AInteractionTutorialGameMode();
+};
+
+
+
